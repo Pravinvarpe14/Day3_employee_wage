283ff8c1035f1f60a32921b07c6d096a7f099e0b
Pravin Varpe <pravinvarpe14@gmail.com>
2022-07-28 22:48:55 +0530

    added employee attendace programme

--- src/employee_wage.java
+++ src/employee_wage.java
@@ -1,10 +1,45 @@
-
+/*
+ * employee wage calculation
+ */
 public class employee_wage {
+	
+	
 
 	public static void main(String[] args) {
-		
-		
+		/*
+		 * welcome message
+		 */
 		System.out.println("Welcome to employee wage computation programme");
+		/*
+		 * taking variable
+		 */
+		int isPresent = 1;
+		/*
+		 * using math.floor 
+		 */
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		
+		/*
+		 * verifying by if else statement
+		 */
+		
+		if (empCheck == isPresent) {
+		
+			
+			System.out.println("Employee is present...");
+		} else {
+			
+			System.out.println("Employee is absent...");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
